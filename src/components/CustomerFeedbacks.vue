a7d5ef05b38d7b521e52dc2b9cd5036975e34e26
ruhiambelkar17 <ruhiambelkar17@users.noreply.github.com>
2023-01-22 23:26:50 +0530

    added feedback component
    added feedback component to view customer feedbacks

--- src/components/CustomerFeedbacks.vue
+++ src/components/CustomerFeedbacks.vue
@@ -4,12 +4,43 @@
     <div class="feedback_container">
         <h1>Customer Feedbacks</h1>
     </div>
+    <div>
+        <div class="row">
+            <div class="col-md-8 offset-2 border mt-3 shadow rounded" v-for="i in arr" :key="i">
+                <p id="name"><b>{{ i.attributes.Name }}</b></p>
+                <hr>
+                <p><b>Email:</b>{{ i.attributes.Email }}</p>
+                <p><b>Mobile No.: </b>{{ i.attributes.PhoneNumber }}</p>
+                <p><b>Feedback:</b>{{ i.attributes.Message }}</p>
+            </div>
+        </div>
+    </div>
 
     <footer-comp></footer-comp>
 
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            name: '',
+            email: '',
+            phone: null,
+            message: '',
+            arr:[],
+            
+        }
+    },
+    mounted(){
+        fetch('http://localhost:1337/api/feedbacks')
+        .then((response) => response.json())
+        .then((data) => {
+            this.arr=data.data;
+            console.log('output:',this.arr);
+        });  
+    }        
+}
 
 </script>
 
@@ -26,5 +57,21 @@
 .feedback_container h1 {
     text-align: center;
     padding-top: 100px;
+}
+
+hr{
+    height: 2px;
+    width: 100px;
+    background-color: red;
+    margin: 0px;
+    margin-bottom: 5px;
+}
+
+#name{
+    margin-top: 10px;
+}
+
+p {
+    color: gray;
 }
 </style>
